53bc3b8358ab97c15db7d64b9037da91d87fd2d2
inesvar <90340779+inesvar@users.noreply.github.com>
2023-01-03 15:26:35 +0100

    Smart pointers yay

--- MediaGroup.h
+++ MediaGroup.h
@@ -1,11 +1,14 @@
 #include <list>
+#include <memory>
 #include "AbstractMedia.h"
+
+typedef std::shared_ptr<AbstractMedia> AbstractMediaPtr;
 
 /**
  * @brief Represents a group of multimedia objects
  * 
 */
-class MediaGroup : public std::list<AbstractMedia*> {
+class MediaGroup : public std::list<AbstractMediaPtr> {
 
 	private:
 		/**
@@ -20,7 +23,7 @@ class MediaGroup : public std::list<AbstractMedia*> {
 		 * 
 		 * @param filename 
 		*/
-		MediaGroup(std::string filename) : std::list<AbstractMedia*>(), _filename(filename){};
+		MediaGroup(std::string filename) : std::list<AbstractMediaPtr>(), _filename(filename){};
 
 		/**
 		 * @brief Get the name of the multimedia group
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #ifdef PART8
 
 #include <iostream>
+#include <memory>
 #include "AbstractMedia.h"
 #include "Video.h"
 #include "Photo.h"
@@ -13,8 +14,8 @@ int main(int argc, const char* argv[]) {
 
     MediaGroup * mg = new MediaGroup("Kabuki");
 
-    mg->push_back(new Video("VIDEO", "./kabuki.gif", 10));
-    mg->push_back(new Photo("PHOTO", "./kabuki.jpg", 1, 2.4));
+    mg->push_back(std::make_shared<Video>("VIDEO", "./kabuki.gif", 10));
+    mg->push_back(std::make_shared<Photo>("PHOTO", "./kabuki.jpg", 1, 2.4));
 
     std::cout << "GROUP : " << mg->getName() << std::endl;
 
@@ -27,13 +28,13 @@ int main(int argc, const char* argv[]) {
 
     MediaGroup * mgJapon = new MediaGroup("Japon");
 
-    mgJapon->push_back(new Photo("PHOTO", "./kabuki.jpg", 1, 2.4));
-    mgJapon->push_back(new Photo("PHOTO", "./sakura.jpg", 1, 2.4));
-    mgJapon->push_back(new Photo("PHOTO", "./kanji.png", 1, 2.4));
+    mgJapon->push_back(std::make_shared<Photo>("PHOTO", "./kabuki.jpg", 1, 2.4));
+    mgJapon->push_back(std::make_shared<Photo>("PHOTO", "./sakura.jpg", 1, 2.4));
+    mgJapon->push_back(std::make_shared<Photo>("PHOTO", "./kanji.png", 1, 2.4));
     
     unsigned int numberOfChapters = 5;
     unsigned int chapters[] = {2,5,2,2,4,2};
-    mgJapon->push_back(new Film("FILM", "./kabuki.gif", 15, numberOfChapters, chapters));
+    mgJapon->push_back(std::make_shared<Film>("FILM", "./kabuki.gif", 15, numberOfChapters, chapters));
 
     std::cout << "GROUP : " << mgJapon->getName() << std::endl;
 
