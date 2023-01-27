a72ec2647842fc9d088238dd464bba3ed66344c6
inesvar <90340779+inesvar@users.noreply.github.com>
2023-01-27 19:37:13 +0100

    little comments

--- window/Window.java
+++ window/Window.java
@@ -15,15 +15,17 @@ public class Window extends JFrame implements ActionListener
 	{
 		super("Window");
 		setSize(700, 700);
-		setDefaultCloseOperation(3);
+		setDefaultCloseOperation(3); // EXIT ON CLOSE
 		setLayout(new BorderLayout());
-
-		textArea = new JTextArea(50,50);
+		
+		// scroll text area
+		textArea = new JTextArea(50,50); // 50 lignes, 50 colonnes
 		textArea.setEditable(false);
 		add(textArea, BorderLayout.CENTER);
 		scrollPane = new JScrollPane(textArea);
 		add(scrollPane, BorderLayout.CENTER);
 
+		// three buttons in a panel
 		panel1 = new JPanel();
 		panel1.setLayout(new GridLayout(1, 3));
 		button1 = new JButton("content");
